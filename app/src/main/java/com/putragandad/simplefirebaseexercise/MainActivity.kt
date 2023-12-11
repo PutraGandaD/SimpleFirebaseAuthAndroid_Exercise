6fa54333ffdee86fc8c9b3c727492bf8665ff9e9
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-12-11 21:51:00 +0700

    Added SignInActivity and DashboardActivity

--- app/src/main/java/com/putragandad/simplefirebaseexercise/MainActivity.kt
+++ app/src/main/java/com/putragandad/simplefirebaseexercise/MainActivity.kt
@@ -2,10 +2,21 @@ package com.putragandad.simplefirebaseexercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.analytics
+import com.putragandad.simplefirebaseexercise.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+
+        }
+
 }
